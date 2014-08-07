a0adaf23ee4520570056b90a1581371ed231a760
Brad Thomason <brad@badfeather.com>
2014-08-07 12:57:23 -0400

    changed minified script suffix to -min to reflect update in codekit, fixed call in scripts.php

--- inc/scripts.php
+++ inc/scripts.php
@@ -2,21 +2,6 @@
 /**
  * Enqueue scripts and styles
  */
-function _bfn_scripts() {
-	wp_enqueue_style( 'bfn-style', get_stylesheet_uri() );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-
-
-
-}
-add_action( 'wp_enqueue_scripts', '_bfn_scripts' );
-
-/**
- * Enqueue scripts and styles
- */
 function bfn_scripts() {
 	wp_enqueue_style( 'bfn-style', get_stylesheet_uri() );
 
@@ -25,10 +10,10 @@ function bfn_scripts() {
 	}
 
 	// including modernizr and whatever else you might need via codekit prepend, calling from bower_components
-	wp_enqueue_script( 'bfn-vendor', get_template_directory_uri() . '/js/min/vendor-ck.js', array(), null, false );
+	wp_enqueue_script( 'bfn-vendor', get_template_directory_uri() . '/js/min/vendor-min.js', array(), null, false );
 
 	// use codekit to prepend any required plugins, which hopefully are available via bower
-	wp_enqueue_script( 'bfn-scripts', get_template_directory_uri() . '/js/min/scripts-ck.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'bfn-scripts', get_template_directory_uri() . '/js/min/scripts-min.js', array( 'jquery' ), null, true );
 
 }
 add_action( 'wp_enqueue_scripts', 'bfn_scripts' );
@@ -43,6 +28,6 @@ add_action( 'wp_head', 'bfn_header_scripts' );
 function bfn_header_scripts() { ?>
 
 <!--[if (gte IE 6)&(lte IE 8)]>
-  <script src="<?php echo get_template_directory_uri(); ?>/js/min/ie-polyfills-ck.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/min/ie-polyfills-min.js"></script>
 <![endif]-->
 <?php } // function bf_header_scripts
