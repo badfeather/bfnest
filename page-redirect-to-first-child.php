9e0be19aa089938dd8062e24d530f7c251387163
Brad Thomason <brad@badfeather.com>
2014-08-08 22:52:32 -0400

    added redirect to first child page template

---
+++ page-redirect-to-first-child.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Redirect To First Child
+*/
+
+global $post;
+$page_children = get_pages( array(
+  'child_of' => $post->ID,
+  'sort_column' => 'menu_order'
+) );
+$first_child = $page_children[0];
+
+wp_redirect( get_permalink( $first_child->ID ) );
+
+
