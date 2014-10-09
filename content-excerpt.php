9b9b590406a53eb135698a60c5b1990733f6b985
Brad Thomason <brad@badfeather.com>
2014-10-08 20:28:37 -0400

    updated node dependencies

--- content-excerpt.php
+++ content-excerpt.php
@@ -1,8 +1,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'excerpt' ); ?>>
 
 	<header class="entry-header">
+	  <?php nest_meta_above(); ?>
 		<h1 class="entry-title excerpt-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-		<?php get_template_part( 'meta-above' ); ?>
 	</header><!-- /.doc-header -->
 
 	<div class="entry-summary">
