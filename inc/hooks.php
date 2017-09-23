0cdd702ed2f5931986ce5900d029ec09b6338cda
Brad Thomason <brad@badfeather.com>
2017-09-23 12:48:12 -0400

    removed unnecessary documentation

--- inc/hooks.php
+++ inc/hooks.php
@@ -1,47 +1,31 @@
 <?php
 /**
  * Adds custom classes to the array of body classes.
- *
- * @param array $classes Classes for the body element.
- * @return array
  */
 function nest_body_classes( $classes ) {
 
-	// @codingStandardsIgnoreStart
-	// Allows for incorrect snake case like is_IE to be used without throwing errors.
 	global $is_IE;
 
-	// If it's IE, add a class.
 	if ( $is_IE ) {
 		$classes[] = 'ie';
 	}
-	// @codingStandardsIgnoreEnd
 
-	// Give all pages a unique class.
 	if ( is_page() ) {
 		$classes[] = 'page-' . basename( get_permalink() );
 	}
 
-	// Adds a class of hfeed to non-singular pages.
 	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
 	}
 
-	// Adds a class of group-blog to blogs with more than 1 published author.
 	if ( is_multi_author() ) {
 		$classes[] = 'group-blog';
 	}
 
-	// Are we on mobile?
-	// PHP CS wants us to use jetpack_is_mobile instead, but what if we don't have Jetpack installed?
-	// Allows for using wp_is_mobile without throwing an error.
-	// @codingStandardsIgnoreStart
 	if ( wp_is_mobile() ) {
 		$classes[] = 'mobile';
 	}
-	// @codingStandardsIgnoreEnd
 
-	// Adds "no-js" class. If JS is enabled, this will be replaced (by javascript) to "js".
 	$classes[] = 'no-js';
 
 	return $classes;
