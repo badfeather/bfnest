50845a1c342e6c346db3c2a198bf309ccdc68036
Brad Thomason <brad@badfeather.com>
2026-04-22 10:50:33 -0400

    udpated dependencies

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,9 +1,5 @@
-// import './expander-new.js';
-// import './menu-button-func.js';
-// import './menu-button.js';
 import './expander-menu.js';
 import './expander.js';
-// import './menu-focus.js';
 // import './color-scheme.js';
 // import './share-popups.js';
 
